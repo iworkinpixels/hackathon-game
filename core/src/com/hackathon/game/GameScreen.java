7343e00e2988412f6a0d1a8aaedee2c0f90701c1
Tomasz Jagodowski <munduss@gmail.com>
2016-02-22 01:17:05 +0100

    screen improvement

--- core/src/com/hackathon/game/GameScreen.java
+++ core/src/com/hackathon/game/GameScreen.java
@@ -3,19 +3,30 @@ package com.hackathon.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by tjago on 2016-02-21.
  */
 public class GameScreen implements Screen {
     private GameStage stage;
+    OrthographicCamera camera;
+    Viewport viewport;
 
     public GameScreen() {
         stage = new GameStage();
+
+        camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+        viewport = new FillViewport(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT, camera);
+        viewport.apply();
     }
 
     @Override
     public void render(float delta) {
+
+        camera.update();
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.draw();
         stage.act(delta);
@@ -28,6 +39,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+
+        viewport.update(width,height);
+        camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
 
     }
 
--- desktop/src/com/hackathon/game/desktop/DesktopLauncher.java
+++ desktop/src/com/hackathon/game/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.hackathon.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hackathon.game.Constants;
 import com.hackathon.game.HackathonGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.width = Constants.VIEWPORT_WIDTH;
+		config.height = Constants.VIEWPORT_HEIGHT;
 		new LwjglApplication(new HackathonGame(), config);
 	}
 }
