1044965cb9611420574f2131dfbc6080e1eef14e
Tomasz Jagodowski <munduss@gmail.com>
2016-02-29 22:48:26 +0100

    fixed Soldier Animation - Scott's update

--- core/src/com/hackathon/game/actors/pleeps/SoldierPleep.java
+++ core/src/com/hackathon/game/actors/pleeps/SoldierPleep.java
@@ -13,16 +13,32 @@ public class SoldierPleep extends BasePleep {
     }
 
     @Override
-    Animation initWalkAnimation() {
-        TextureRegion[] walkframes = new TextureRegion[7];
-        walkframes[0] = spritePosition[5][3];
-        walkframes[1] = spritePosition[5][4];
-        walkframes[2] = spritePosition[5][5];
-        walkframes[3] = spritePosition[5][6];
-        walkframes[4] = spritePosition[5][7];
-        walkframes[5] = spritePosition[5][8];
-        walkframes[6] = spritePosition[5][9];
-
+    Animation initWalkAnimation() { //carry bomb
+        TextureRegion[] walkframes = new TextureRegion[4];
+        walkframes[0] = spritePosition[5][6];
+        walkframes[1] = spritePosition[5][7];
+        walkframes[2] = spritePosition[5][8];
+        walkframes[3] = spritePosition[5][9];
         return new Animation(0.1f, walkframes);
     }
+
+
+    Animation initThrowBombAnimation() {
+        TextureRegion[] throwframes = new TextureRegion[2];
+        throwframes[0] = spritePosition[5][0];
+        throwframes[1] = spritePosition[5][1];
+        return new Animation(0.1f, throwframes);
+    }
+
+
+    Animation initPanicAnimation() {
+        TextureRegion[] panicframes = new TextureRegion[4];
+        panicframes[0] = spritePosition[5][2];
+        panicframes[1] = spritePosition[5][3];
+        panicframes[2] = spritePosition[5][4];
+        panicframes[3] = spritePosition[5][5];
+        return new Animation(0.1f, panicframes);
+    }
+
+
 }
